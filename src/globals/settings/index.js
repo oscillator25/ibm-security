bee6c55f741762d19144146c3f80cd08ac51383e
Simon Finney <SIMONFIN@ie.ibm.com>
2020-08-20 17:57:16 +0100

    feat(theme): add ability to provide CSS custom property prefix

---
+++ src/globals/settings/index.js
@@ -0,0 +1,6 @@
+/**
+ * @file Settings.
+ * @copyright IBM Security 2020
+ */
+
+export default {};
--- src/globals/theme/index.js
+++ src/globals/theme/index.js
@@ -1,15 +1,17 @@
 /**
  * @file Themes.
- * @copyright IBM Security 2019
+ * @copyright IBM Security 2019 - 2020
  */
 
 import { formatTokenName, g100 } from '@carbon/themes';
+import settings from '../settings';
 
 const theme = {};
 
 // Rewrites the theme using CSS custom properties.
 Object.keys(g100).forEach(token => {
-  theme[token] = `var(--${formatTokenName(token)}, ${g100[token]})`;
+  theme[token] = `var(--${settings.cssCustomPropertyPrefix ||
+    ''}${formatTokenName(token)}, ${g100[token]})`;
 });
 
 export default theme;
