eb51f23d28d3002fe26973e67ae33c3f7a0d55d5
Simon Finney <SIMONFIN@ie.ibm.com>
2021-01-27 14:31:23 +0000

    fix(combo-button): use `ibm-cloud-cognitive` version

--- src/components/ComboButton/ComboButtonItem/index.js
+++ src/components/ComboButton/ComboButtonItem/index.js
@@ -1,8 +1,8 @@
 /**
  * @file Combo button item entry point.
- * @copyright IBM Security 2019
+ * @copyright IBM Security 2019 - 2021
  */
 
-import ComboButtonItem from './ComboButtonItem';
-
-export default ComboButtonItem;
+export {
+  ComboButtonItem as default,
+} from '@carbon/ibm-cloud-cognitive-security/lib/ComboButton/ComboButtonItem';
