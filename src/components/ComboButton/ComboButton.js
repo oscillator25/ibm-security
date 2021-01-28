729be78b898b0ea3e366e548ae91a4641c0b86ce
Simon Finney <SIMONFIN@ie.ibm.com>
2021-01-28 18:50:13 +0000

    build(combo-button): explore prop adapter

--- src/components/ComboButton/ComboButton.js
+++ src/components/ComboButton/ComboButton.js
@@ -4,7 +4,21 @@
  */
 
 import { ComboButton as CarbonComboButton } from '@carbon/ibm-cloud-cognitive-security/lib/ComboButton';
+import merge from 'deepmerge';
+
+import {
+  func,
+  node,
+  number,
+  oneOf,
+  oneOfType,
+  shape,
+  string,
+} from 'prop-types';
+
 import React from 'react';
+
+import { TooltipDirection } from '../IconButton/IconButton';
 
 const ComboButton = ({
   children,
@@ -14,23 +28,105 @@ const ComboButton = ({
   menuOffsetFlip,
   selectorPrimaryFocus,
   ...other
-}) => {
-  return (
-    <CarbonComboButton
-      overflowMenu={{
-        direction,
-        menuOffset,
-        menuOffsetFlip,
-        selectorPrimaryFocus,
-      }}
-      {...other}
-    >
-      {children}
-    </CarbonComboButton>
-  );
+}) => (
+  <CarbonComboButton
+    overflowMenu={{
+      direction,
+      menuOffset,
+      menuOffsetFlip,
+      selectorPrimaryFocus,
+    }}
+    {...other}
+  >
+    {children}
+  </CarbonComboButton>
+);
+
+const { BOTTOM, TOP } = TooltipDirection;
+
+const source = {
+  propTypes: {
+    children: node.isRequired,
+    className: string,
+    direction: oneOf([TOP, BOTTOM]),
+    menuOffset: oneOfType([
+      shape({
+        left: oneOf([number, string]),
+        top: oneOf([number, string]),
+      }),
+      func,
+    ]),
+    menuOffsetFlip: oneOfType([
+      shape({
+        left: oneOf([number, string]),
+        top: oneOf([number, string]),
+      }),
+      func,
+    ]),
+    selectorPrimaryFocus: string,
+  },
+  defaultProps: {
+    className: '',
+    direction: TOP,
+    menuOffset: () => ({
+      left: 'auto',
+    }),
+    menuOffsetFlip: undefined,
+    selectorPrimaryFocus: '[data-overflow-menu-primary-focus]',
+  },
 };
 
-const { defaultProps, propTypes } = CarbonComboButton;
+const getDifference = (source, target) =>
+  Object.keys(source).reduce(
+    (difference, key) =>
+      target[key] === source[key]
+        ? difference
+        : {
+            ...difference,
+            [key]: source[key],
+          },
+
+    {}
+  );
+
+const createAdapter = ({ source, target, transformer }) => {
+  const toTransform = {
+    source: {
+      propTypes: getDifference(source.propTypes, target.propTypes),
+      defaultProps: getDifference(source.defaultProps, target.defaultProps),
+    },
+    target: {
+      propTypes: getDifference(target.propTypes, source.propTypes),
+      defaultProps: getDifference(target.defaultProps, source.defaultProps),
+    },
+  };
+
+  console.log(toTransform);
+
+  const transform = transformer(toTransform);
+
+  const { propTypes, defaultProps } = transform;
+
+  return {
+    propTypes: merge(target.propTypes, propTypes),
+    defaultProps: merge(target.defaultProps, defaultProps),
+  };
+};
+
+const { defaultProps, propTypes } = createAdapter({
+  source,
+  target: CarbonComboButton,
+  transformer: ({ source, target }) => ({
+    propTypes: {},
+    defaultProps: {
+      className: source.defaultProps.className,
+      overflowMenu: {
+        direction: source.defaultProps.direction,
+        menuOffset: source.defaultProps.menuOffset,
+      },
+    },
+  }),
+});
 
 ComboButton.propTypes = propTypes;
 ComboButton.defaultProps = defaultProps;
