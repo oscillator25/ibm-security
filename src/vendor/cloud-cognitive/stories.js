b57d2efa41a1b10c6ca2667bb90fe3b4b787d9b7
Simon Finney <SIMONFIN@ie.ibm.com>
2021-04-21 20:36:06 +0100

    feat(cloud-cognitive): proxy `Cloud & Cognitive` package

--- src/vendor/cloud-cognitive/stories.js
+++ src/vendor/cloud-cognitive/stories.js
@@ -3,11 +3,30 @@
  * @copyright IBM Security 2021
  */
 
+import React from 'react';
+
 import { cloudCognitive } from '../../../.storybook';
 import { dependencies } from '../../../package.json';
 
+import { CloudCognitive } from '../..';
+
 const { '@carbon/ibm-cloud-cognitive': version } = dependencies;
 
-export const CLOUD_COGNITIVE = 'Cloud & Cognitive';
+const {
+  pkg: { component },
+} = CloudCognitive;
+
+const CLOUD_COGNITIVE = 'Cloud & Cognitive';
+const Components = () => (
+  <ul>
+    {Object.keys(component)
+      .filter(key => component[key])
+      .map(component => (
+        <li key={`li__${component}`}>{component}</li>
+      ))}
+  </ul>
+);
 
 export default () => cloudCognitive(version);
+
+export { CLOUD_COGNITIVE, Components };
